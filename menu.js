3e565b81b99d3297e16686851515a0cfac9101eb
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-09-05 10:54:12 +0000

    Menu.js updated

---
+++ menu.js
@@ -0,0 +1,9 @@
+console.log('Welcome to Monicredit')
+console.log('\n Enter Your Choice')
+console.log('\n 1. Create new account')
+console.log('\n 2. Deposit Money')
+console.log('\n 3. Withdraw money')
+console.log('\n 4. Check Balance')
+console.log('\n 5. Transfer Money')
+console.log('\n 6. Exit')
+
