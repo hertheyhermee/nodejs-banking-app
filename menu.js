0159043703d7a5764d6968970680228408882f26
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-09-05 12:05:51 +0100

    menu.js upate

--- menu.js
+++ menu.js
@@ -1,5 +1,4 @@
 console.log('Welcome to Monicredit')
-console.log('\n Enter Your Choice')
 console.log('\n 1. Create new account')
 console.log('\n 2. Deposit Money')
 console.log('\n 3. Withdraw money')
@@ -7,3 +6,6 @@ console.log('\n 4. Check Balance')
 console.log('\n 5. Transfer Money')
 console.log('\n 6. Exit')
 
+const readline = require('readline')
+
+console.log('\n Enter Your Choice: ')
